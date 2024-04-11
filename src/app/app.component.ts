08567b00fd98a7faabb8bed1dbad3ed78203021e
eesnault1 <erwanesnault1@gmail.com>
2024-04-11 10:05:32 +0200

    chore: add reset CSS and clean Angular project to keep essentials

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,8 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: '<h1>Tour of Heroes</h1>',
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'heroes';
